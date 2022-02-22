cca115c84916c402deef4507add866b9a200e166
Dan Smith <dsmith@danplanet.com>
2022-02-21 17:28:48 -0800

    Auth fix was 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gaiagpsclient',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=['requests', 'prettytable', 'pytz', 'tzlocal', 'pyyaml', 'pathvalidate'],
     entry_points={
